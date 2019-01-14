bdb3a2cd3511952572e1f0f7a3832d482a6161d2
github username <github email address>
2019-01-14 15:18:13 -0500

    Added Provider to index.js to render app component as well as set store

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ import * as serviceWorker from './serviceWorker';
 const store = createStore(rootReducer)
 
 ReactDOM.render(
-   
+   <Provider store={store}>
     <App />, 
-   
+    </Provider>,
 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
