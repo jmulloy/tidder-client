eebd5149c239848a4069001f3458a859d45f5a8f
github username <github email address>
2019-01-14 15:30:51 -0500

    added welcome Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Navbar.js'
-
+import Welcome from './components/Welcome.js'
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Header className="Header"/>
       <div className='App-Header'>
+        <Welcome />
       <h1>Posts Board</h1>
           <img src={logo} className="App-logo" alt="logo" />
        </div>
---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+      <div className='welcome-page'>
+        <h1>Welcome to the Posts React App</h1>
+        {/* <p>Welcome to my world</p> */}
+      </div>
+    )
+  }
+
+  export default Welcome
