acb8964e04ba563c96a6d48dcbafb1d770e31d6e
github username <github email address>
2019-01-15 08:54:13 -0500

    changed p tag to h3 and h4 tags in the about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,9 @@ const About = () => {
     return (
       <div className='about-page'>
 
-        <h2>Tidder can be utilized for creating quick posts consiting of ideas and thoughts giving users the option to comment on posts should they choose to!</h2>
-        <p>This app was built using React and Redux for the frontend functionality. I also incorporated the use of retrieving data from a rails API on the backend of the application</p>
-        <p>This is an app I built for the Final Project in the fullstack Web Development curriculum at the Flatiron School!</p>
+        <h2>Tidder can be utilized for creating quick posts consiting of ideas and thoughts. While also giving users the option to comment on posts should they choose to!</h2>
+        <h3>This app was built using React and Redux for the frontend functionality. I also incorporated the use of retrieving data from a rails API on the backend of the application</h3>
+        <h4>This is an app I built for the Final Project in the fullstack Web Development curriculum at the Flatiron School!</h4>
       </div>
     )
   }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,6 @@ const Welcome = () => {
     return (
       <div className='welcome-page'>
         <h1>Welcome to the Tidder React App</h1>
-        {/* <p>Welcome to my world</p> */}
       </div>
     )
   }
