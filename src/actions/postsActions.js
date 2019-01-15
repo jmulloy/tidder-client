0986631e566b514679e48c72b1811274a47f60e0
github username <github email address>
2019-01-15 14:08:14 -0500

    Added single post fetch in actions file

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -6,3 +6,12 @@ export function fetchPosts() {
         .then(posts => dispatch({type: 'FETCH_POSTS', posts}))
     }
 }
+
+export function fetchSinglePosts(postId) {
+    return(dispatch) => {
+        dispatch({type: 'START_ADDING_POSTS_REQUEST'})
+        return fetch('http://localhost:3005/api/posts/${postId}')
+        .then(response => response.json())
+        .then(post => dispatch({type: 'ADD_SINGLE_POST', post}))
+    }
+}
---
+++ src/containers/PostsDisplayContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class PostsDisplayContainer extends Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
