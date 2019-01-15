21de196ae1acb9e479aeed1c35da98a20dea2392
github username <github email address>
2019-01-15 14:01:31 -0500

    Added Link to tags and path for the PostDebut file

--- src/components/PostDebut.js
+++ src/components/PostDebut.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PostDebut = (props) => {
     return (
         <div className="block" key={post.id} >
-        <h4>{props.post.title}</h4>
+        <h4><Link to='/posts/${props.post.id}'>{props.post.title}</Link></h4>
         <p>{props.post.author}</p>
       </div>
     )
