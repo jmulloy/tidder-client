d2fc0a240eeaf8761ad3380de6e930a30a0bf0d6
github username <github email address>
2019-03-06 09:26:56 -0500

    Updated SinglePost Component to be a class component

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const SinglePost = (props) => {
-    
+class SinglePost extends Component {
+render() {
     return (
         <div className="post-block">
-            <h2>{props.post.title}</h2>
-            <h3>{props.post.author}</h3>
-            <p>{props.post.content}</p> 
+            <h2>{this.props.post.title}</h2>
+            <h3>{this.props.post.author}</h3>
+            <p>{this.props.post.content}</p> 
         </div>
     )
 }
-
+}
 export default SinglePost
+
+// const SinglePost = (props) => {
+    
+//     return (
+//         <div className="post-block">
+//             <h2>{props.post.title}</h2>
+//             <h3>{props.post.author}</h3>
+//             <p>{props.post.content}</p> 
+//         </div>
+//     )
+// }
